9c1a8135ea6020a4be6ac3205ec32ff195b42308
terahashi <tkshplant@outlook.jp>
2025-11-25 22:16:40 +0900

    TailwindCSSを追加 設定完了

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,12 @@ import HeroscrambleText from '../components/HeroscrambleText';
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home トップページです。</h1>
+    <div className='bg-blue-500 w-full h-screen'>
+      <h1 className='bg-[#da5555] !m-0'>Home トップページです。</h1>
       <HeroscrambleText />
+      <div className='bg-custom-yellow'>
+        <h1>子要素(背景黄色はbg-custom-yellowの「Tailwindcssのカスタム」)</h1>
+      </div>
     </div>
   );
 };
