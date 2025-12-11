e0693df3d8b71e71f5cddac15433df70a09e1bfe
terahashi <tkshplant@outlook.jp>
2025-12-11 18:19:20 +0900

    App.jsx修正2 -  スクロール位置を保存コード修正

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,4 +1,5 @@
 import { useRef, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { styled } from 'styled-components';
 import animateHamburger from '../animations/HamburgerAnimation'; //hamburger.jsからGSAPアニーションをimport
 
