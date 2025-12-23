9d7d1e07ab5d695d7cc2ace5fd1d707308c9bdfd
terahashi <tkshplant@outlook.jp>
2025-12-23 21:01:17 +0900

    WorksItem.jsx preload=metadataを削除

--- src/components/WorksItem.jsx
+++ src/components/WorksItem.jsx
@@ -6,7 +6,7 @@ const WorksItem = ({ work, onHover }) => {
     <div className='w__item' key={work.id} onMouseEnter={() => onHover(work)} onMouseLeave={() => onHover(null)}>
       <Link to={`/works/${work.id}`}>
         {/* <img src={work.image} alt={work.title} /> */}
-        <video src={work.videos} preload='metadata' autoPlay muted loop playsInline />
+        <video src={work.videos} autoPlay muted loop playsInline />
         {/* preload="metadata" で「動画の長さやサイズ を最低限だけ取得」 */}
       </Link>
       {/* 今回使用した{`/works/${work.id}`} ⬅︎この書き方はテンプレートリテラルであり「URLルール(遷移先)が"コード側にある"」*/}
