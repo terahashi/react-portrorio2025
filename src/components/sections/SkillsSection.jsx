06b107293daeebd0ccb680ee30de25aa2d917343
terahashi <tkshplant@outlook.jp>
2026-01-12 21:04:58 +0900

    Nodeのバージョンが新しすぎた(v22)ことが原因でrun devでブラウザ起動不可になった。Nodeを安定版20 LTSに戻しnode_modulesを作り直した。復旧完了

--- src/components/sections/SkillsSection.jsx
+++ src/components/sections/SkillsSection.jsx
@@ -2,6 +2,8 @@ import { Wrapper, Inner } from '../common/LayoutPrimitives';
 import styled from 'styled-components';
 
 import ParagraphSection from '../ParagraphSection';
+
+import FadeUp from '../FadeUp';
 import SideFixed from '../SideFixed';
 import Skillslist from '../SkillsList';
 
@@ -20,6 +22,25 @@ const SkillsSection = ({ headerRef }) => {
         <Wrapper>
           <Inner>
             <ParagraphSection title='Skills' />
+
+            <FadeUp
+              options={{
+                y: 40,
+                stagger: 0.1,
+                duration: 0.4,
+                glitch: true,
+              }}
+            >
+              <div className='split'>
+                {'SKILLS'.split('').map((char, i) => (
+                  <span key={i} className='char'>
+                    {char}
+                  </span>
+                ))}
+              </div>
+              <p className='char'>スクロール</p>
+            </FadeUp>
+
             <SideFixed
               title='追従タイトル追従タイトル追従タイトル'
               sideText='Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi nemo voluptate, dolor numquam exercitationem quod obcaecati sunt sint commodi laborum. Accusantium, ea. Deserunt dolorum, recusandae consequuntur nisi harum possimus quod.'
